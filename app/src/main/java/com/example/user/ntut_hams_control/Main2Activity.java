49b6ee60b1045ac3a689d839ff1975aebb167551
yinghai <yinghai97@hotmail.com>
2018-09-25 20:58:17 +0800

    nothing

--- app/src/main/java/com/example/user/ntut_hams_control/Main2Activity.java
+++ app/src/main/java/com/example/user/ntut_hams_control/Main2Activity.java
@@ -1,18 +1,20 @@
 package com.example.user.ntut_hams_control;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
-    private Button testButton;
     private ConstraintLayout accountlayout;
     private ProgressBar loadingBar;
+    private EditText UsernameET;
+    private EditText PasswordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +27,28 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //隐藏bar的code
         Button Loginbutton = findViewById(R.id.Loginbutton);
         Loginbutton.setOnClickListener(this);
-        testButton = findViewById(R.id.testbutton);
         accountlayout = findViewById(R.id.accountLayout);
         loadingBar = findViewById(R.id.loadingBar);
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                accountlayout.setVisibility(View.VISIBLE);
-                loadingBar.setVisibility(View.INVISIBLE);
+        UsernameET = findViewById(R.id.UsernameET);
+        PasswordET = findViewById(R.id.PasswordET);
 
-            }
-        });
+
     }
 
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.Loginbutton) {
+            String username = UsernameET.getText().toString();
+            String password = PasswordET.getText().toString();
 
-            Intent i = new Intent(this, MainActivity.class);
-            startActivity(i);
+
+
+//            Intent i = new Intent(this, MainActivity.class);
+//            startActivity(i);
         }
     }
+
+
+
 }
